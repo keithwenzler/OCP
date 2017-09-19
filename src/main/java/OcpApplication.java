64abfd3daa740de79018eb3f5a985f30d4895d00
Keith Wenzler <keith.wenzler@passporthealth.com>
2017-09-19 16:28:14 -0500

    predicate lambda example

--- src/main/java/OcpApplication.java
+++ src/main/java/OcpApplication.java
@@ -1,7 +1,21 @@
+import java.util.function.Predicate;
+
 public class OcpApplication {
 
     public static void main(String[] args) {
-        System.out.println("hello keith");
+
+        OcpApplication ocpApplication = new OcpApplication();
+        ocpApplication.test(6, i -> i > 5);
+
+    }
+
+    private void test(int count, Predicate<Integer> integerPredicate)  {
+
+        if (integerPredicate.test(count)) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
     }
 
 }
