103d35500ee7283b3722a65ae1c7a15c5e060b22
Keith Wenzler <keith.wenzler@passporthealth.com>
2017-09-19 16:49:00 -0500

    my changes

--- src/main/java/OcpApplication.java
+++ src/main/java/OcpApplication.java
@@ -1,21 +1,23 @@
-import java.util.function.Predicate;
+import classes.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OcpApplication {
 
     public static void main(String[] args) {
 
-        OcpApplication ocpApplication = new OcpApplication();
-        ocpApplication.test(6, i -> i > 5);
-
-    }
-
-    private void test(int count, Predicate<Integer> integerPredicate)  {
-
-        if (integerPredicate.test(count)) {
-            System.out.println("pass");
-        } else {
-            System.out.println("fail");
+        List<Test> testList = new ArrayList<>();
+        for (int i = 0; i < 1000 ; i++) {
+            testList.add(new Test(i + 1));
         }
+
+        long count = testList.stream()
+                .filter(t -> t.getID() > 500)
+                .count();
+
+        System.out.println(count);
+
     }
 
 }
