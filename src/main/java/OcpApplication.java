90c26cc66dda848959159bdc89a8ab0724cc6b5a
Keith Wenzler <keith.wenzler@passporthealth.com>
2017-09-20 17:43:34 -0500

    my changes

--- src/main/java/OcpApplication.java
+++ src/main/java/OcpApplication.java
@@ -1,19 +1,46 @@
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Function;
 
 public class OcpApplication {
 
     public static void main(String[] args) {
 
-        boolean b1 = false;
-        boolean b2 = false;
+        OcpApplication ocpApplication = new OcpApplication();
+        ocpApplication.process();
 
-//        Stream<Boolean> results = Stream.of(b1, b2);
+    }
 
-        boolean results = Stream.of(b1, b2).anyMatch(b -> b.equals(true));
+    private boolean isNullOrEmpty(String input) {
+        return !Optional.ofNullable(input).filter(s -> s.length() > 0).isPresent();
+    }
 
-        System.out.println(results);
+    private static Function<String, Boolean> isNullOrEmpty2() {
+        return s1 -> !Optional.ofNullable(s1).filter(s2 -> s2.length() > 0).isPresent();
+    }
+
+    private boolean isAnyNullOrEmpty(String... inputs) {
+        return Arrays.stream(inputs)
+            .anyMatch(s -> s == null || s.length() == 0);
+    }
 
 
+    private void process() {
+
+        String s1 = "1";
+        String s2 = "1";
+        String s3 = "1";
+
+        System.out.println(!isAnyNullOrEmpty(s1, s2, s3));
+
+    }
+
+    private boolean greaterThanFour(String letter) {
+        if (letter.length() > 4) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
